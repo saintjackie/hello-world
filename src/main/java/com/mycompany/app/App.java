63623ed654b30c9d06398d683d437f767a486807
Jakub Holy <436353@mail.muni.cz>
2015-10-28 15:16:34 +0100

    3. zmena z netbeans

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hellow git-hub. Whats up, this is first push!");
+        System.out.println( "Git-hub. Whats up, this is 3. push!");
     }
 }
