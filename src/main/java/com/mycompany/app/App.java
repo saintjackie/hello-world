55785bb0fc6662eeb8286362f6d5af6a0f0b4b67
Jakub Holy <saintjackie@gmail.com>
2015-10-28 15:27:11 +0100

    5. zmena z netbeans

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Git-hub. Whats up, this is 4. push! Changed name and email!");
+        System.out.println( "Git-hub. Name changed from saintjackie to Jakub Holy!");
     }
 }
