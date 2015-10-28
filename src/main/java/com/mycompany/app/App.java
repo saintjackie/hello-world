b24405e19727a7ff0946f32a3a366fab56c1271b
Jakub Holy <436353@mail.muni.cz>
2015-10-28 14:48:27 +0100

    2. zmena z netbeans

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello PA165!");
+        System.out.println( "Hellow git-hub. Whats up, this is first push!");
     }
 }
